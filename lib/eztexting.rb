184f1f42cad8215fc5954a8ca5e511f34932faa4
Jason Harrelson <jason@lookforwardenterprises.com>
2011-04-29 13:56:35 -0500

    Allow for adaptable URL to allow use of grouptexting.com.

--- lib/eztexting.rb
+++ lib/eztexting.rb
@@ -9,21 +9,30 @@ require 'eztexting/voice'
 
 module Eztexting
   include HTTParty
-  
+
   # Sets up the connection that the other classes
   # use to be able communicate with the api
-  def self.connect!(username,password)
+  def self.connect!(username,password,uri="https://app.eztexting.com/api")
     # HTTParty Method to store base url
-    self.base_uri "https://app.eztexting.com/api"
-    
+
+    raise "Invalid URI" unless valid_uris.include?( uri )
+    self.base_uri uri
+
     # Catch the username and password for use in all calls
     @username = username
-    @password  = password 
+    @password  = password
     puts "credentials cached"
   end
-  
+
+  def self.valid_uris
+    %w(
+      https://app.eztexting.com/api
+      https://app.grouptexting.com/api
+    )
+  end
+
   def self.credentials
     {:user => @username, :pass => @password}
   end
-  
+
 end
